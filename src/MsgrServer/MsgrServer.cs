af06a0fa4a6ee6ac0d2d36a83c27e8cc29ee5089
exec <exec@mabinoger.de>
2015-09-18 15:11:43 +0200

    Websocket test

--- src/MsgrServer/MsgrServer.cs
+++ src/MsgrServer/MsgrServer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Aura development team - Licensed under GNU GPL
 // For more information, see license file in the main folder
 
+using Aura.Mabi.Network;
 using Aura.Msgr.Database;
 using Aura.Msgr.Network;
 using Aura.Msgr.Util;
@@ -8,6 +9,8 @@ using Aura.Shared;
 using Aura.Shared.Util;
 using Aura.Shared.Util.Commands;
 using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace Aura.Msgr
 {
@@ -35,16 +38,22 @@ namespace Aura.Msgr
 		public MsgrConf Conf { get; private set; }
 
 		/// <summary>
+		/// Msgr's packet handlers
+		/// </summary>
+		public MsgrServerHandlers PacketHandlerManager { get; private set; }
+
+		/// <summary>
 		/// Initializes msgr server.
 		/// </summary>
 		private MsgrServer()
 		{
 			this.Database = new MsgrDb();
 			this.Conf = new MsgrConf();
-
 			this.Server = new MsgrServerServer();
-			this.Server.Handlers = new MsgrServerHandlers();
-			this.Server.Handlers.AutoLoad();
+
+			this.PacketHandlerManager = new MsgrServerHandlers();
+			this.PacketHandlerManager.AutoLoad();
+			this.Server.Handlers = this.PacketHandlerManager;
 		}
 
 		public void Run()
@@ -67,10 +76,47 @@ namespace Aura.Msgr
 			// Start
 			this.Server.Start(this.Conf.Msgr.Port);
 
+			var ws = new WebSocketServer("ws://127.0.0.1:8181");
+			ws.AddWebSocketService<MsgrBehavior>("/");
+			ws.Start();
+
 			CliUtil.RunningTitle();
 
 			var cmd = new ConsoleCommands();
 			cmd.Wait();
+		}
+	}
+
+	public class MsgrBehavior : WebSocketBehavior
+	{
+		protected override void OnOpen()
+		{
+			Shared.Util.Log.Debug("Connection opened.");
+		}
+
+		protected override void OnMessage(MessageEventArgs e)
+		{
+			//Shared.Util.Log.Debug("Msg: " + e.Data);
+			Shared.Util.Log.Debug("RawMsg: " + BitConverter.ToString(e.RawData));
+
+			var packet = new Packet(e.RawData, 0);
+			Shared.Util.Log.Debug(packet);
+
+			packet = new Packet(Op.Msgr.LoginR, 0);
+			packet.PutByte(true);
+			packet.PutInt(0x12345678);
+
+			this.Send(packet.Build());
+		}
+
+		protected override void OnClose(CloseEventArgs e)
+		{
+			Shared.Util.Log.Debug("Connection closed.");
+		}
+
+		protected override void OnError(ErrorEventArgs e)
+		{
+			Shared.Util.Log.Debug("Error: " + e.Message);
 		}
 	}
 }
